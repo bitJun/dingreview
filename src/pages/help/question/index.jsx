fc7a2b692b24b7171a6b99635c5360fbc9bb6a66
徐俊俊 <609924321@qq.com>
2019-11-21 14:59:22 +0800

    更换成Taro-ui

--- src/pages/help/question/index.jsx
+++ src/pages/help/question/index.jsx
@@ -1,6 +1,10 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
-import { ClAccordion } from "mp-colorui";
+import {
+  AtAccordion,
+  AtList,
+  AtListItem
+} from 'taro-ui';
 
 class ServiceGuarantee extends Component {
   config = {
@@ -9,6 +13,7 @@ class ServiceGuarantee extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      open: false,
       questions: [
         {
           question: '如何使用洛客云设计？',
@@ -88,32 +93,35 @@ class ServiceGuarantee extends Component {
       ]
     }
   }
-  renderContent(card, animation, speed = 0.15) {
-    return (
-      <ClLayout margin='normal' marginDirection='vertical'>
-        <ClAccordion title='道德经' card={card} animation={animation} speed={speed}>
-          <ClLayout padding='normal' paddingDirection='around'>
-            <ClCard type='full'>
-              <ClText text='道可道，非常道。名可名，非常名。无名天地之始。有名万物之母。' textColor='blue' />
-            </ClCard>
-            <ClCard type='full'>
-              <ClText text='有无相生，难易相成，长短相形，高下相盈，音声相和，前后相随。恒也。' textColor='brown' />
-            </ClCard>
-            <ClCard type='full'>
-              <ClText text='上善若水。水善利万物而不争，处众人之所恶，故几于道。' textColor='red' />
-            </ClCard>
-            <ClCard type='full'>
-              <ClText text='道生一，一生二，二生三，三生万物。万物负阴而抱阳，冲气以为和。' />
-            </ClCard>
-          </ClLayout>
-        </ClAccordion>
-      </ClLayout>
-    )
+  handleClick (value) {
+    this.setState({
+      open: value
+    })
   }
   render () {
+    const {
+      open,
+      questions
+    } = this.state;
     return (
       <View>
-        {this.renderContent(false, true)}
+        {
+          questions.map(item=>
+            <AtAccordion
+              open={open}
+              onClick={this.handleClick.bind(this)}
+              title={item.question}
+            >
+              <AtList hasBorder={false}>
+                <AtListItem
+                  title={item.answer}
+                  arrow='right'
+                  thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+                />
+              </AtList>
+            </AtAccordion>
+          )
+        }
       </View>
     )
   }
